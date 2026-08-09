ea9c8f999abf3c247244908e1bb0183e7fcbbc77
manuel <tirocinio@manuel.pc>
2026-08-09 11:32:50 +0200

    gitignore aggiornato

--- visualization/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Plot di confronto tra traiettoria GPS (UTM) e traiettoria stimata dalla VO."""
-
-from .trajectory_plot import plot_trajectories, plot_gps_only
-
-__all__ = ["plot_trajectories", "plot_gps_only"]
